0a1c012c584238dac46c8928722bce2e1840cdd1
ernest <gyeduernest@gmail.com>
2024-02-09 15:25:36 +0000

    first commit with components added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Signup from "./Components/Form"
+
+
+
+function App() {
+
+  return (
+    <>
+    <div className=" flex justify-center p-10">
+        <Signup/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Alerts.jsx
@@ -0,0 +1,11 @@
+import { Alert } from 'flowbite-react';
+
+function Alerts() {
+  return (
+    <Alert color="info">
+      <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
+  );
+}
+
+export default Alerts;
