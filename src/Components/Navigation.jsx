0aae021b081216245d33fd8ede2f436285c88e76
ernest <gyeduernest@gmail.com>
2024-02-11 01:31:41 +0000

    cards done

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,7 +3,7 @@ import AvatarDropdown from "./AvatarDropdown";
 import { Button, TextInput } from 'flowbite-react';
 import { HiSearch } from 'react-icons/hi';
 import Swap from "./Toggle";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import Dashboard from "../Pages/Dashboard";
 import Search from "../Pages/Search";
 import Signup from "../Pages/Signup"
@@ -15,14 +15,13 @@ import Donation from "../Pages/Donation";
 
 
 export function Navigation() {
-  
-  
+
   
   
   return (
         <Router>
     <>
-    <div className="lg:p-2  md:p-10 sm:p-3 bg-slate-50 w-full fixed">
+    <div className="lg:p-2  md:p-10 sm:p-3 bg-slate-50 w-full fixed z-50">
 
       
       {/* TOP NAVIGATION */}
@@ -61,21 +60,21 @@ export function Navigation() {
 
       <div className="fixed py-10 px-5 bg-slate-50 lg:h-full lg:w-64  z-50 hidden sm:hidden md:hidden lg:block">
           <ul>
-           <Link to="/dashboard">
-            <li className="flex gap-4 items-center rounded-md w-40 bg-teal-200 p-2 mb-10">
+           <NavLink to="/dashboard" className=""  >
+            <li className="flex gap-4 items-center rounded-md w-40  p-2 mb-10 "  >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
             <p className="text-sm font-bold ">
-              Home  
+              Dashboard  
             </p>   
             </li>
-          </Link>
+          </NavLink>
 
 
-          <Link to="/search">
+          <NavLink to="/search">
 
-            <li className="mb-10 flex gap-4 items-center rounded-md w-40 bg-teal-200 p-2">
+            <li className="mb-10 flex gap-4 items-center rounded-md w-40  p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
@@ -83,12 +82,12 @@ export function Navigation() {
               Search  
             </p>   
             </li>
-          </Link>
+          </NavLink>
 
-          <Link to="/projects">
+          <NavLink to="/projects">
 
 
-            <li className="mb-10 flex gap-4 items-center rounded-md w-40 bg-teal-200 p-2">
+            <li className="mb-10 flex gap-4 items-center rounded-md w-40  p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
             </svg>
@@ -96,12 +95,12 @@ export function Navigation() {
               Projects  
             </p>   
             </li>
-          </Link>
+          </NavLink>
 
 
-          <Link to="/donation">
+          <NavLink to="/donation">
 
-            <li className="mb-10 flex gap-4 items-center rounded-md w-40 bg-teal-200 p-2">
+            <li className="mb-10 flex gap-4 items-center rounded-md w-40  p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
               </svg>
@@ -110,11 +109,11 @@ export function Navigation() {
               Donation  
             </p>   
             </li>
-          </Link>
+          </NavLink>
 
 
-          <Link to="/settings">
-            <li className="mb-10 flex gap-4 items-center rounded-md w-40 bg-teal-200 p-2">
+          <NavLink to="/settings">
+            <li className="mb-10 flex gap-4 items-center rounded-md w-40  p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
               <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -123,7 +122,7 @@ export function Navigation() {
               Settings  
             </p>   
             </li>
-          </Link>
+          </NavLink>
             <li>
               <Swap/>
             </li>
@@ -136,8 +135,8 @@ export function Navigation() {
   {/* 
       MOBILE AND TABLET NAVIGATION */}
 
-          <div className="flex md:flex px-5 py-3 bg-slate-50 top-[600px] relative md:h-20 sm:px-16  h-20 gap-8 sm:flex md:gap-40 sm:gap-20 md:px-20 lg:hidden">
-        <Link to="/dashboard">
+          <div className="flex md:flex px-10 py-3 bg-slate-50 top-[600px] relative md:h-20 sm:px-16  h-20 gap-8 sm:flex md:gap-40 sm:gap-20 md:px-20 lg:hidden">
+        <NavLink to="/dashboard">
 
           <Button color="gray" size="sm" className="h-10 w-10">
              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -145,9 +144,9 @@ export function Navigation() {
             </svg>
 
           </Button>
-        </Link>
+        </NavLink>
 
-        <Link to="/search">
+        <NavLink to="/search">
 
           <Button color="gray" className="h-10 w-10" size="sm">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -155,20 +154,20 @@ export function Navigation() {
             </svg>
 
           </Button>
-        </Link>
+        </NavLink>
 
-        <Link to="/projects">
+        <NavLink to="/projects">
 
           <Button color="gray" size="sm" className="h-10 w-10">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
             </svg>
           </Button>
-        </Link>
+        </NavLink>
 
 
 
-        <Link to="/donation">
+        <NavLink to="/donation">
 
           <Button color="gray" size="sm" className="h-10 w-10">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -176,10 +175,10 @@ export function Navigation() {
               </svg>
 
           </Button>
-        </Link>
+        </NavLink>
 
 
-        <Link to="/settings">
+        <NavLink to="/settings">
 
           <Button color="gray" size="sm" className="h-10 w-10">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -188,7 +187,7 @@ export function Navigation() {
             </svg>
 
           </Button>
-        </Link>
+        </NavLink>
 
           
 
